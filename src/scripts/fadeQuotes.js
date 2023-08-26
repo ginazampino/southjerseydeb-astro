26b03140781c06b34c36c4fa0aaff0e5d593d602
Gina <9094115+ginazampino@users.noreply.github.com>
2023-08-26 18:29:49 -0400

    Work towards contact and testimonials.

---
+++ src/scripts/fadeQuotes.js
@@ -0,0 +1,31 @@
+export function fadeQuotes(selector) {
+    const list = Array.from(document.querySelectorAll(selector));
+    console.log(list);
+    const delay = 5000;
+
+    const fadeBehavior = () => {
+        list.forEach((item, i) => {
+            setTimeout(() => {
+                showItem(item);
+            }, delay * i);
+
+            setTimeout(() => {
+                hideItem(item);
+            }, (delay * i) + delay);
+        });
+    };
+
+    const showItem = (item) => {
+        item.classList.add('active');
+    };
+
+    const hideItem = (item) => {
+        item.classList.remove('active');
+    };
+
+    fadeBehavior();
+
+    setInterval( () => {
+        fadeBehavior()
+    }, delay * list.length);
+};
